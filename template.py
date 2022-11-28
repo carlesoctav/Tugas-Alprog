15adeda19a15142ff6418846163b1ab2a9836202
cxrles <CarlesDevelopment19@gmail.com>
2022-11-28 17:17:00 +0700

    Create template.py

---
+++ template.py
@@ -0,0 +1,19 @@
+# Input
+n=int(input())
+A=[]
+for i in range(n):
+    row_i= [int(i) for i in input().split(' ')]
+    A.append(row_i)
+####
+
+# masukkan algoritma anda. 
+
+
+    
+
+
+
+
+# Output
+hasil= ...
+print(hasil)
